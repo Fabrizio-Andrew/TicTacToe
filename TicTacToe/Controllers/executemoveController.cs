e084f5992866e533f3782f390ff857b6cecd127c
Andrew <andrewdfabrizio@gmail.com>
2021-02-03 21:15:53 -0500

    Updated class ints to be nullable, renamed executemoveController to spec

---
+++ TicTacToe/Controllers/executemoveController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TicTacToe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class executemoveController : ControllerBase
+    {
+
+
+        // POST api/<GameController>
+        [HttpPost]
+        public string ExecuteMoveResponse([FromBody] ExecuteMove messagePayload)
+        {
+            string response = CalculateResponse.CalculateMoveResponse(messagePayload);
+
+            return response;
+
+            
+        }
+    }
+}
---
+++ TicTacToe/DataTransferObjects/ExecuteMoveResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class ExecuteMoveResponse
+    {
+        public int? move { get; set; }
+
+        public char azurePlayerSymbol { get; set; }
+
+        public char humanPlayerSymbol { get; set; }
+
+        public char winner { get; set; }
+
+        public int[] winPositions { get; set; }
+
+        public char[] gameBoard { get; set; }
+    }
+}
