8b5a4d7a68277772a8a0822a367d3d4ee96c07fd
Andrew <andrewdfabrizio@gmail.com>
2021-02-07 15:44:37 -0500

    Connected front & back ends.

--- TicTacToe/wwwroot/TicTacTron.js
+++ TicTacToe/wwwroot/TicTacTron.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const boardPositions = document.querySelectorAll(".box");
     for (i = 0; i < boardPositions.length; i++) {
         const box = boardPositions[i];
@@ -6,16 +6,86 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
+// Global Variables
+let humanSymbol = "X";
+let azureSymbol = "O";
+let URL = "https://localhost:5001/api/executemove";
+
+// Prevents user from submitting another move before azure has responded.
 let azureThinking = false;
 
-function PlayerMove(box) {
+function PlayerMove(playerPosition) {
     
     if (azureThinking != true) {
-        box.setAttribute("value", "X");
-        box.innerHTML = 'X';
-        box.style.animationPlayState = "running";
+        playerPosition.setAttribute('data-value', humanSymbol);
+        playerPosition.innerHTML = humanSymbol;
+        playerPosition.style.animationPlayState = "running";
+        playerPosition.onclick = null;
         azureThinking = true;
-        // Fetch goes here
-        azureThinking = false;
+
+        // Post user's move to API and handle response
+        fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                move: playerPosition.id,
+                azurePlayerSymbol: azureSymbol,
+                humanPlayerSymbol: humanSymbol,
+                gameBoard: [
+                    document.getElementById('0').getAttribute('data-value'),
+                    document.getElementById('1').getAttribute('data-value'),
+                    document.getElementById('2').getAttribute('data-value'),
+                    document.getElementById('3').getAttribute('data-value'),
+                    document.getElementById('4').getAttribute('data-value'),
+                    document.getElementById('5').getAttribute('data-value'),
+                    document.getElementById('6').getAttribute('data-value'),
+                    document.getElementById('7').getAttribute('data-value'),
+                    document.getElementById('8').getAttribute('data-value'),
+                ]
+            })
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+                setTimeout(() => AzureMove(result), 1500);
+
+
+            });
     }
+}
+
+function AzureMove(response) {
+
+    if (response.move != null) {
+        const azurePosition = document.getElementById(response.move)
+
+        azurePosition.setAttribute('data-value', azureSymbol)
+        azurePosition.onclick = null;
+
+        azurePosition.style.color = 'red';
+        azurePosition.style.animationName = 'azure-move';
+        azurePosition.style.animationPlayState = 'running';
+        azurePosition.innerHTML = azureSymbol;
+
+        if (response.winner == azureSymbol) {
+            console.log(`winner: ${azureSymbol}`);
+
+        } else if (response.winner == 'tie') {
+            console.log('Tie.')
+
+        } else {
+            azureThinking = false;
+        }
+    }
+    else if (response.winner == humanSymbol) {
+        console.log(`winner: ${humanSymbol}`);
+    }
+
+    else {
+        console.log(`Tie.`);
+    }
+
 }
