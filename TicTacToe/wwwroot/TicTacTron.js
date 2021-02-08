079f60f0ad7440b2b879ca3c153dee5a1c1200c4
Andrew <andrewdfabrizio@gmail.com>
2021-02-08 15:28:17 -0500

    Front end comments/formatting

--- TicTacToe/wwwroot/TicTacTron.js
+++ TicTacToe/wwwroot/TicTacTron.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Set onclick properties for gameboard positions
     const boardPositions = document.querySelectorAll(".box");
     for (i = 0; i < boardPositions.length; i++) {
         const box = boardPositions[i];
         box.onclick = () => PlayerMove(box);
     }
+
+    // Set onclick properties for start menu symbol selection
     const selectBoxes = document.querySelectorAll(".select-box");
     for (i = 0; i < selectBoxes.length; i++) {
         const selectBox = selectBoxes[i];
@@ -16,25 +20,34 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 // API Endpoint
-let URI = "https://localhost:5001/api/executemove";
+let URI = "/api/executemove";
 
 // Global Variables
 let humanSymbol = "?";
 let azureSymbol = "?";
 
 
-// Prevents user from submitting another move before azure has responded.
+// Flag prevents user from submitting another move before azure has responded.
 let azureThinking = false;
 
+// Handles the animation and API call for the user's inputs
 function PlayerMove(playerPosition) {
     
     if (azureThinking != true) {
+
+        azureThinking = true;
+
+        // Set humanSymbol in space
         playerPosition.setAttribute('data-value', humanSymbol);
         playerPosition.innerHTML = humanSymbol;
+
+        // Run animation & audio
         playerPosition.style.animationPlayState = "running";
         document.getElementById('playermove-audio').play();
+
+        // Prevent player from clicking the same box again
         playerPosition.onclick = null;
-        azureThinking = true;
+
 
         // Post user's move to API and handle response
         fetch(URI, {
@@ -68,20 +81,26 @@ function PlayerMove(playerPosition) {
     }
 }
 
+// Handles the render and animation of the azurePlayers move received via API response
 function AzureMove(response) {
 
     if (response.move != null) {
         const azurePosition = document.getElementById(response.move)
 
+        // Set azureSymbol in space
+        azurePosition.innerHTML = azureSymbol;
         azurePosition.setAttribute('data-value', azureSymbol)
-        azurePosition.onclick = null;
 
+        // Run animation & audio
         azurePosition.style.color = 'red';
         azurePosition.style.animationName = 'azure-move';
         azurePosition.style.animationPlayState = 'running';
         document.getElementById('azuremove-audio').play();
-        azurePosition.innerHTML = azureSymbol;
 
+        // Prevent player from clicking this box
+        azurePosition.onclick = null;
+
+        // Display messaging for Azure win or tie
         if (response.winner == azureSymbol) {
             document.getElementById('game-over').innerHTML = '<p>You lose.  End of Line.</p>';
         } else if (response.winner == 'tie') {
@@ -89,21 +108,20 @@ function AzureMove(response) {
         } else {
             azureThinking = false;
         }
-    }
-    else if (response.winner == humanSymbol) {
+    // Display messaging for human win or tie
+    } else if (response.winner == humanSymbol) {
         document.getElementById('game-over').innerHTML = '<p>You win.  End of Line.</p>';
-    }
-
-    else {
+    } else {
         document.getElementById('game-over').innerHTML = '<p>Tie.</p>';
     }
 }
 
 function SymbolSelect(symbol) {
 
+    // Set the humanSymbol value
     humanSymbol = symbol;
-    console.log(symbol);
 
+    // Set azureSymbol value to opposite of humanSymbol.  Hide corresponding select box.
     if (symbol == 'X') {
         document.getElementById('select-box-O').style.display = 'none';
         azureSymbol = 'O';
@@ -112,6 +130,7 @@ function SymbolSelect(symbol) {
         azureSymbol = 'X';
     }
 
+    // Run animation/audio - hides start menu and displays game board
     document.getElementById('symbol-container').style.animationPlayState = 'running';
     document.getElementById('gamestart-audio').play();
 
